7feeba5803a702dec6959d80ad8acbda07b97d72
gatetep10 <p.gatete1@gmail.com>
2020-08-25 11:36:24 +0200

    Update manage_cow.php

--- manage_cow.php
+++ manage_cow.php
@@ -31,7 +31,7 @@ include_once('side_bar.php');
             <!-- DataTales -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Doners Table</h6>
+              <h6 class="m-0 font-weight-bold text-primary">Cows Table</h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
